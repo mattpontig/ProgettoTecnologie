b1dcc08cacd9ce8d9b0371acb91870460c35e047
IodiceAle <118894160+IodiceAle@users.noreply.github.com>
2022-12-14 17:25:13 +0100

    aggiunto server con collegamento db

---
+++ Server/gestoreDB.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class gestoreDB {
+    public static void connetti() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/db_telegram", "root", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from login");
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
